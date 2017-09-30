58b35268b9981db53eb6980f3dc5a67bfa8a0d22
dreamensys <dreamensys@gmail.com>
2017-09-30 06:59:31 -0500

    IPathFinder was added

---
+++ HoundMazePathFinder/Algorithms/IPathFinder.cs
@@ -0,0 +1,11 @@
+﻿namespace HoundMazePathFinder.Algorithms
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    public interface IPathFinder
+    {
+        List<Point> FindPath();
+        bool Search(Node currentNode)
+    }
+}
